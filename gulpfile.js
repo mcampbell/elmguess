31c910c5db2ef8012a48c8fac73be35b46845be4
Michael Campbell <michael.campbell@gmail.com>
2016-08-05 21:11:36 -0400

    feat: build works
    * used the functions from the knowthen tutorial for gulpfile build.
    * Put in a bullshit helloworld html to make sure the gulpfile server
      works.
    * confirmed `elm-make` works to make a `bundle.js` file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,43 @@
 /* File: gulpfile.js */
 
-// grab our gulp packages
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+'use strict';
 
-// create a default task and just log a message
-gulp.task('default', function() {
-    return gutil.log('Gulp is running!');
+// grab our gulp packages
+var gulp = require('gulp'),
+    http = require('http'),
+    st = require('st'),
+    exec = require('child_process').exec,
+    gutil = require('gulp-util'),
+    clear = require('clear');
+
+// What do run to do our compile
+var cmd = 'elm make ./src/Main.elm --output ./bundle.js';
+
+gulp.task('default', ['server', 'watch', 'elm']);
+
+// Taken from https://github.com/knowthen/elm/tree/master/05%20scorekeeper-starter
+gulp.task('watch', function() {
+    gulp.watch('**/*.elm', ['elm']);
+});
+
+gulp.task('server', function(done) {
+    gutil.log(gutil.colors.blue('Starting server at http://localhost:4000'));
+    http.createServer(
+        st({
+            path: __dirname + '/static',
+            index: 'index.html',
+            cache: false
+        })
+    ).listen(4000, done);
+});
+
+gulp.task('elm', function(cb) {
+    exec(cmd, function(err, stdout, stderr) {
+        if (err){
+            gutil.log(gutil.colors.red('elm make: '),gutil.colors.red(stderr));
+        } else {
+            gutil.log(gutil.colors.green('elm make: '), gutil.colors.green(stdout));
+        }
+        cb();  // what does this do
+    });
 });
